2e219a9bb2ef4b3603b8cba8b8ea32cc17b60f88
Eren Erdogan <erenerdogan12323@gmail.com>
2025-09-19 17:50:15 +0300

    text editor in c++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    //--//
+    QStringList args = QApplication::arguments();
+    if (args.size() > 1) w.loadFile(args[1]);
+    else w.loadFile("");
+    //--//
+
+    return a.exec();
+}
